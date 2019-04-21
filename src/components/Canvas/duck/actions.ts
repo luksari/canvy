9d900ce0115458fb946494d6ea1b3656bbdd5ae5
luksari <luksari23@gmail.com>
2019-04-21 11:56:01 +0200

    Remodeled reducer, actions

--- src/components/Canvas/duck/actions.ts
+++ src/components/Canvas/duck/actions.ts
@@ -1,22 +1,23 @@
 import { createStandardAction } from 'typesafe-actions';
 
 import { START_DRAWING, END_DRAWING, DRAWING } from './constants'
+import { Point } from 'MyModels';
 
 export const startDrawing = createStandardAction(START_DRAWING)
     .map(
-        (prevX: number, prevY: number) => ({
-            payload: { prevX, prevY, isDrawing: true }
+        (prevPoint : Point) => ({
+            payload: { prevPoint, isDrawing: true }
         })
     );
 export const drawing = createStandardAction(DRAWING)
     .map(
-        (prevX: number, prevY: number) => ({
-            payload: { prevX, prevY, isDrawing: true}
+        (prevPoint : Point) => ({
+            payload: { prevPoint, isDrawing: true}
         })
     )
 export const endDrawing = createStandardAction(END_DRAWING)
     .map(
-        (prevX: number, prevY: number) => ({
-            payload: { prevX, prevY, isDrawing: false }
+        (prevPoint : Point) => ({
+            payload: { prevPoint, isDrawing: false }
         })
     );
--- src/components/Canvas/duck/reducer.ts
+++ src/components/Canvas/duck/reducer.ts
@@ -4,12 +4,12 @@ import { RootAction } from 'MyTypes';
 import { ActionType } from 'typesafe-actions';
 import * as constants from './constants'
 import * as canvas from './actions';
+import { Point } from 'MyModels';
 
 
 export type CanvasState = {
     isDrawing: boolean,
-    prevX: number,
-    prevY: number
+    prevPoint: Point 
 
 }
 export type CanvasAction = ActionType<typeof canvas>
@@ -28,28 +28,15 @@ export const canvasReducer = combineReducers<CanvasState, RootAction>({
                 return state
         }
     },
-    prevX: (state: number = 0, action: CanvasAction) => {
+    prevPoint: (state: Point = {x: 0, y: 0}, action : CanvasAction) => {
         const { START_DRAWING, DRAWING, END_DRAWING } = constants
         switch(action.type) {
             case START_DRAWING:
-                return action.payload.prevX
+                return action.payload.prevPoint
             case DRAWING:
-                return action.payload.prevX
+                return action.payload.prevPoint
             case END_DRAWING:
-                return action.payload.prevX
-            default:
-                return state;
-        }
-    },
-    prevY: (state: number = 0, action: CanvasAction) => {
-        const { START_DRAWING, DRAWING, END_DRAWING } = constants
-        switch(action.type) {
-            case START_DRAWING:
-                return action.payload.prevY
-            case DRAWING:
-                return action.payload.prevY
-            case END_DRAWING:
-                return action.payload.prevY
+                return action.payload.prevPoint
             default:
                 return state
         }
