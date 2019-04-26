028ca78f971b0b572f4a7a8d32030803b0036faf
luksari <luksari23@gmail.com>
2019-04-26 12:33:10 +0200

    Created Toolbar Reducer

--- src/components/Toolbar/duck/reducer.ts
+++ src/components/Toolbar/duck/reducer.ts
@@ -1,6 +1,31 @@
-import { RootAction } from 'MyTypes'
+
+import { RootAction, RootState } from 'MyTypes';
+import { ActionType } from 'typesafe-actions';
+import * as constants from './constants'
+import * as toolbar from './actions';
+import { Point } from 'MyModels';
+import { combineReducers } from 'redux';
 
 export type ToolbarState = {
     color: string,
     thickness: number,
 }
+
+export type ToolbarAction = ActionType<typeof toolbar>
+
+export const toolbarReducer = combineReducers<RootState, RootAction>({
+    color: (state : string = '#BADA55', action : ToolbarAction) => {
+        const { SELECT_COLOR } = constants
+        switch(action.type){
+            case SELECT_COLOR: return action.payload;
+            default: return state;
+        }
+    },
+    thickness: (state: number = 3, action: ToolbarAction) => {
+        const { SELECT_THICKNESS } = constants
+        switch(action.type){
+            case SELECT_THICKNESS: return action.payload;
+            default: return state;
+        }
+    }
+})
