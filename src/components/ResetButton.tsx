7d0aa26028deaff1c035a94ff06d5cd759253be4
luksari <luksari23@gmail.com>
2019-05-12 20:01:38 +0200

    Working on styles of Toolbar

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { RootState } from 'MyTypes';
 import { connect } from 'react-redux';
 import { resetCanvas } from '../containers/Toolbar/duck/actions';
+import styled from 'styled-components';
 
 const mapDispatchToProps = {
   resetCanvas,
@@ -9,15 +10,36 @@ const mapDispatchToProps = {
 
 type Props = typeof mapDispatchToProps;
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledButton = styled.button`
+  border: none;
+  width: 80px;
+  max-width: 150px;
+  padding: 10px;
+  height: 45px;
+  background: #fff;
+  border-radius: 2px;
+  box-shadow: 0 4px 5px 0 rgba(173, 173, 173, 0.59);
+  cursor: pointer;
+`;
 const ResetButtonRaw: React.FunctionComponent<Props> = ({ resetCanvas }) => {
   return (
-    <button
-      onClick={() => {
-        resetCanvas(true);
-      }}
-    >
-      Reset
-    </button>
+    <Wrapper>
+      <StyledButton
+        onClick={() => {
+          resetCanvas(true);
+        }}
+        onTouchStart={() => {
+          resetCanvas(true);
+        }}
+      >
+        Reset
+      </StyledButton>
+    </Wrapper>
   );
 };
 
